b2d9904f1bce66da66938f0bd52f145d09302d38
NeneNafe <nenechiwaya@gmail.com>
2023-03-17 14:50:10 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print number starting from zero
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
