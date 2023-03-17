e7bfec5538add59c8678336ceb9143ead23545cf
NeneNafe <nenechiwaya@gmail.com>
2023-03-17 03:39:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /*
  * main - A program that prints the size of various types
  *
- * Return
+ * Return Always 0 (Success)
  */
 int main(void)
 {
@@ -18,3 +18,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu btye(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
