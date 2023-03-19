1f7d10771a12c57c7855676f4d23dd9afa139469
NeneNafe <nenechiwaya@gmail.com>
2023-03-19 08:57:07 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <swtdio.h>
+
+/**
+ * main - Print Dora Korpar's words
+ * Return: Always Success
+ *
+ */
+
+int main(void)
+
+{
+char;
+scanf("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+return (1);
+}
