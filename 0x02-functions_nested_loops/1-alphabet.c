80e33d181f8aff8592b3ce3b636dee1c8f9aeb26
NeneNafe <nenechiwaya@gmail.com>
2023-03-21 04:43:00 -0700

    a function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet, in lowercase followed by a new line
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 1.0x02. C - Functions, nested loops
 2. 0. _putchar
+3. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
