7573fdee52fd08bf9b10abfbf27483336c781e7b
NeneNafe <nenechiwaya@gmail.com>
2023-03-23 08:46:14 -0700

    A program that prints integers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - Prints the numbers, from 0 to 9, followed by a new line
- * Return: Always (Success)
+ * Return: void
  */
 
 void print_numbers(void)
@@ -10,8 +10,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	printf("%d", i);
+	putchar(i);
 	}
+	putchar('\n');
 }
