c95a873ccf398e4fa85f2cf9b7197372260c0ae4
NeneNafe <nenechiwaya@gmail.com>
2023-03-29 10:40:55 -0700

    a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - reverse array
- * @n: integer paramater
+ * @n: integer params
  * Return: 0
  */
 
@@ -17,6 +17,7 @@ void rev_string(char *n)
 		i++;
 	}
 	i--;
+
 	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
@@ -26,12 +27,12 @@ void rev_string(char *n)
 }
 
 /**
- * *infinite_add - adds two numbers
- * @n1: number one
- * @n2: number
- * @r: buffer
- * @size_r: Buffer size
- * Return: 0 if result is not equal to r
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -52,11 +53,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + i) - '\0';
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + j) - '\0';
+			val2 = *(n2 + j) - '0';
 		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
