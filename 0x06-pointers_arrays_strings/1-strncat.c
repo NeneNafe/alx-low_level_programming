d729e247c5442bd6804cea7f9d04d9df5e4e6d0b
NeneNafe <nenechiwaya@gmail.com>
2023-03-29 03:09:40 -0700

    a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenate two strings
+ * @dest: Destination
+ * @src: String to be copied
+ * @n: An integer
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
