c85b64ff60e254c769370b56fbc635707bf94eaf
NeneNafe <nenechiwaya@gmail.com>
2023-04-09 14:31:02 -0700

    Fixed betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
-* @argc: argument count
-* @argv: argument vector
-* Return: Always Success
-*/
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always Success
+ */
 
 int main(int argc, char *argv[])
 {
 	int count;
+
 	for (count = 0; count < argc; count++)
 		printf("%s\n", argv[count]);
 	return (0);
