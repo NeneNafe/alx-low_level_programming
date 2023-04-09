62739add6ed1db61a4d6f856a8cb114fea78479c
NeneNafe <nenechiwaya@gmail.com>
2023-04-09 13:38:22 -0700

    a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - a program that prints itself
+ * @argc: argument count parameter
+ * @argv: argument vector
+ * Return: Always Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	(void)argc;
+	return (0);
+}
