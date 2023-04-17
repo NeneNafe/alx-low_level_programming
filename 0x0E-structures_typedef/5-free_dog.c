4f0aef6b6652767c864fb65d3d8fd5bea87ae96a
NeneNafe <nenechiwaya@gmail.com>
2023-04-17 06:54:03 -0700

    a function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to the function
+ * Return: Nothing.
+ */
+
+void free_dog(dog_t *d)
+{
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		free(d);
+}
