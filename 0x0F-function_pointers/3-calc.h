bb2d73c9ab9dad4a86a33d62ceb62bc0d05aba94
NeneNafe <nenechiwaya@gmail.com>
2023-04-19 14:27:24 -0700

    A file containing all prototypes for this function

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
 #ifndef CALC_H
-#define CLAC_H
+#define CALC_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,7 +15,6 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-
 } op_t;
 
 int op_add(int a, int b);
@@ -27,7 +26,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 
 int op_mod(int a, int b);
-
-int (*get_op_func(char *s))(int, int);
 
 #endif
