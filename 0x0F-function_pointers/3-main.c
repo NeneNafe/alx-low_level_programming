3ae7e6a833d444f74723fd0f8b60a0a91d45e536
NeneNafe <nenechiwaya@gmail.com>
2023-04-19 13:46:03 -0700

    A main.c file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "calc.h"
+
+/**
+ * main - prints all arguments
+ *
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: Success.
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	f = get_op_func(argv[2]);
+
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	printf("%d\n", f(num1, num2));
+
+	return (0);
+}
