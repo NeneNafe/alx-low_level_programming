0d844b5b854d5100aeff8b8daf97577cb4158c86
NeneNafe <nenechiwaya@gmail.com>
2023-04-19 13:47:53 -0700

    this file contains the function that selects the correct function to perform the operation asked by the user

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "calc.h"
+
+/**
+ * *get_op_func - pointer to function of a string
+ * with integer parameters
+ * @s: the string
+ * Return: Success.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (s[0] == ops[i].op[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
