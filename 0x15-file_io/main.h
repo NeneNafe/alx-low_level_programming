c3a319ea23c9f688a022d3de48639892047c6800
NeneNafe <nenechiwaya@gmail.com>
2023-05-08 15:18:36 -0700

    modified

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,4 +16,15 @@ int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int _strlen(char *s);
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_cladd(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_asabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 #endif
