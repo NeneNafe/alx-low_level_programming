2a5b65cfbbd754361a0b2a3c021b9469d74bd52a
NeneNafe <nenechiwaya@gmail.com>
2023-10-06 05:33:28 -0700

    changes

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
 /**
  * shash_table_create - creates a sorted hash table
  * @size: the size of the array
