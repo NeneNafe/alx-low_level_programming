4042f8d2de1ec1a182b07c6ea32266868c5a3ae1
NeneNafe <nenechiwaya@gmail.com>
2023-10-06 01:22:31 -0700

    made hanges

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,41 +13,41 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 	char *value_copy;
-	unsigned long int indx;
+	unsigned long int indx, i;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || *key == '\0' || value  == NULL)
 		return (0);
-	/* checks if the input values are valid */
 
-	indx = key_index((const unsigned char *)key, ht->size);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 	/* calculates the index where the key-value pair would be stored */
+	indx = key_index((const unsigned char *)key, ht->size);
+	for (i = indx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
 	new_node = malloc(sizeof(hash_node_t));
-	/* creates a new node to hold key and value */
 	if (new_node == NULL)
+	{
+		free(value_copy);
 		return (0);
-	/* the statements dup the key and value to ensure they're safely stored */
+	}
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-	if (value == NULL)
-		value_copy = strdup("");
-	else
-		value_copy = strdup(value);
-
-	if (value != NULL && value_copy == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	/*intitializes the new node */
 	new_node->value = value_copy;
-	new_node->next = NULL;
-	if (ht->array[indx] != NULL)
-		new_node->next = ht->array[indx];
+	new_node->next = ht->array[indx];
 	ht->array[indx] = new_node;
+
 	return (1);
 }
